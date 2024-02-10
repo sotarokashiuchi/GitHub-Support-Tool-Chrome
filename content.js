24de12caa0391894fe1d31fe20bed462a1dd761b
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-02-10 18:00:04 +0900

    based chatGPT

---
+++ content.js
@@ -0,0 +1,39 @@
+// テキストを置換する関数
+function replaceText() {
+    var elements = document.getElementsByTagName('*');
+  
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+  
+      for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+  
+        if (node.nodeType === 3) {
+          var text = node.nodeValue;
+          var replacedText = text.replace("t", "aaaaaaaaaaaaaaaaa"); // 置換する文字列を指定
+  
+          if (replacedText !== text) {
+            element.replaceChild(document.createTextNode(replacedText), node);
+          }
+        }
+      }
+    }
+  }
+  
+  // ページ読み込み時にテキストを置換
+  replaceText();
+  
+  // ページのDOM変更を監視して動的にテキストを置換
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'childList') {
+        replaceText();
+      }
+    });
+  });
+  
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+  
