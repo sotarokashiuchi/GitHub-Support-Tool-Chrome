9d5e7f8575d5a1321913e0c9ddda5341da35d7e6
Kashiuchi Sotaro <sotaro0416@gmail.com>
2024-02-12 19:25:50 +0900

    name serach

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-
+const nameList = [
+  ["KashiuchiSotaroSub", "樫内サブ"], 
+  ["TEST", "test"], 
 ];
 
 // テキストを置換する関数
@@ -9,11 +11,26 @@ function searchName() {
   
   
   for(var i = 0; i < nameList.length; i++){
-    // createElement = createElement + '<option value="' + '"></option>'
     createElement = createElement + '<option value="' + nameList[i][1] + '"></option>'
   }
   elements.insertAdjacentHTML("afterbegin", createElement)
 }
+
+function clickHandlerSearchName(e) {
+  let textInput = document.getElementById('search-box').value;
+  let result = ""
+  for(let i=0; i<nameList.length; i++){
+    if(textInput != nameList[i][1])continue
+    result = nameList[i][0]
+  }
+
+  let textOutput = document.getElementById('resolt-box')
+  textOutput.value = result;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('search-button').addEventListener('click', clickHandlerSearchName);
+});
 
 // ページ読み込み時にテキストを置換
 searchName();
